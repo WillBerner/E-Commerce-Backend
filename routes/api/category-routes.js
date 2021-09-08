ab709ea68071dadb579ff3ba9d8a70cc1bfa0c74
Will Berner <will.o.berner@gmail.com>
2021-09-08 17:55:30 -0400

    Implemented CREATE functionality for all endpoints.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -31,6 +31,14 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const newCategory = await Category.create(req.body);
+
+    res.status(200).json(newCategory);
+
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 router.put('/:id', async (req, res) => {
