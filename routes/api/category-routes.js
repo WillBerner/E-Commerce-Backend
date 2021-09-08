dc2133bb6ad0c63fdd208cfc83f5273fc1e9c16a
Will Berner <will.o.berner@gmail.com>
2021-09-08 18:10:30 -0400

    Implemented UPDATE functionality fot Tag and Category Endpoints.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -33,9 +33,7 @@ router.post('/', async (req, res) => {
   // create a new category
   try {
     const newCategory = await Category.create(req.body);
-
     res.status(200).json(newCategory);
-
   } catch (error) {
     res.status(500).json(error);
   }
@@ -43,6 +41,16 @@ router.post('/', async (req, res) => {
 
 router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const updatedCategory = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      }
+    });
+    res.status(200).json(updatedCategory);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 router.delete('/:id', async (req, res) => {
