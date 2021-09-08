61f6b134d46a4dc7c4b33512b6bbfa13e867b4df
Will Berner <will.o.berner@gmail.com>
2021-09-08 18:14:24 -0400

    Formatted for consistency and readability.

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -5,12 +5,11 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', async (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
   try {
     const productData = await Product.findAll({
       include: [{model: Category}, {model: Tag}],
     });
+
     res.status(200).json(productData);
   } catch (error) {
     res.status(500).json(error);
@@ -19,12 +18,11 @@ router.get('/', async (req, res) => {
 
 // get one product
 router.get('/:id', async (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
   try {
     const productData = await Product.findByPk(req.params.id, {
       include: [{model: Category}, {model: Tag}]
     });
+
     res.status(200).json(productData);
   } catch (error) {
     res.status(500).json(error);
@@ -106,7 +104,6 @@ router.put('/:id', async (req, res) => {
 });
 
 router.delete('/:id', async (req, res) => {
-  // delete one product by its `id` value
   try {
     const productData = await Product.destroy({
       where: { 
